07196ddff91e8304274c162dbac0ec296874ba8d
musto <musti.sar4@gmail.com>
2022-11-04 10:33:42 +0100

    Update

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 let lstMovies = [
 	{
