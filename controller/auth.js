52e143d841495d2f291c1329119b23a5cd73e744
musto <musti.sar4@gmail.com>
2022-11-12 11:56:53 +0100

    Update the documentation.html file

--- controller/auth.js
+++ controller/auth.js
@@ -17,29 +17,31 @@ const generateJWTToken = (user) => {
 /* POST login. */
 module.exports = (router) => {
     router.route("/login")
-        .post((req, res) => {
-            passport.authenticate(
-                "local",
-                { session: false },
-                (error, user, info) => {
-                    if (error || !user) {
-                        return res.status(400)
-                            .json({
-                                message: "Something is not right",
-                                user: user
+        .post(
+            (req, res) => {
+                passport.authenticate(
+                    "local",
+                    { session: false },
+                    (error, user, info) => {
+                        if (error || !user) {
+                            return res.status(400)
+                                .json({
+                                    message: "Something is not right",
+                                    user: user
+                                });
+                        }
+                        req.login(
+                            user,
+                            { session: false },
+                            (error) => {
+                                if (error) {
+                                    res.send(error);
+                                }
+                                const token = generateJWTToken(user.toJSON());
+                                return res.json({ user, token });   // Equal to: res.json({ user: user, token: token });
                             });
                     }
-                    req.login(
-                        user,
-                        { session: false },
-                        (error) => {
-                            if (error) {
-                                res.send(error);
-                            }
-                            const token = generateJWTToken(user.toJSON());
-                            return res.json({ user, token });   // Equal to: res.json({ user: user, token: token });
-                        });
-                }
-            )(req, res);
-        });
+                )(req, res);
+            }
+        );
 }
