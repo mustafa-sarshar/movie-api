96a8b39c0a1c107fc8534caf5bc413f1bc0746c0
musto <musti.sar4@gmail.com>
2022-11-28 11:55:21 +0100

    Upadte

--- index.js
+++ index.js
@@ -3,28 +3,31 @@ require("dotenv").config();
 
 // Requirements
 const express = require("express"),
-    morgan = require("morgan"),
-    fs = require("fs"),
-    path = require("path"),
-    bodyParser = require("body-parser"),
-    methodOverride = require("method-override"),
-    mongoose = require("mongoose"),
-    passport = require("passport"),
-    swaggerUi = require("swagger-ui-express");
+  morgan = require("morgan"),
+  fs = require("fs"),
+  path = require("path"),
+  bodyParser = require("body-parser"),
+  methodOverride = require("method-override"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  swaggerUi = require("swagger-ui-express");
 
 const { requestDateTimeNow } = require("./utils/middleware"),
-    { corsMiddleware } = require("./config/cors"),
-    swaggerJSON = require("./public/swagger");
+  { corsMiddleware } = require("./config/cors"),
+  swaggerJSON = require("./public/swagger");
 
 mongoose.connect(process.env.DATABASE_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 // create a write stream (in append mode) a ‘log.txt’ file is created in root directory
-const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "logs", "log.txt"),
+  {
     flags: "a",
-});
+  }
+);
 
 const app = express();
 
@@ -49,31 +52,31 @@ app.use("/genres", require(path.join(__dirname, "routes", "genres.js")));
 
 // Public Routes
 app.route("/").get((req, res) => {
-    res.sendFile("/public/index.html", { root: __dirname });
+  res.sendFile("/public/index.html", { root: __dirname });
 });
 app.route("/documentation").get((req, res) => {
-    res.sendFile("/public/documentation.html", { root: __dirname });
+  res.sendFile("/public/documentation.html", { root: __dirname });
 });
 
 // Error-handling middleware
 app.use((err, req, res, next) => {
-    console.error("Error:\n", err.stack);
-    res.status(500).send("Something broke!");
+  console.error("Error:\n", err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // Swagger UI for API Documentation
 app.use(
-    "/swagger",
-    swaggerUi.serve,
-    // swaggerUi.setup(swaggerJsdocSpecs, { explorer: true })
-    swaggerUi.setup(swaggerJSON, { explorer: true })
+  "/swagger",
+  swaggerUi.serve,
+  // swaggerUi.setup(swaggerJsdocSpecs, { explorer: true })
+  swaggerUi.setup(swaggerJSON, { explorer: true })
 );
 
 const PORT = process.env.PORT || 3000;
 mongoose.connection.once("open", () => {
-    console.log("Connected to Database");
-    // Start the server and listen to events on port ...
-    app.listen(PORT, "0.0.0.0", () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  console.log("Connected to Database");
+  // Start the server and listen to events on port ...
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
