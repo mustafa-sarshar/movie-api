5854d05e0e131ca6975fdbd78327b15c73a4cd56
musto <musti.sar4@gmail.com>
2022-12-04 20:24:20 +0100

    Update CORS

--- config/cors.js
+++ config/cors.js
@@ -1,13 +1,16 @@
 const cors = require("cors");
 
-// const allowedOrigins = process.env.ALLOWED_ORIGINS.split(";");
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(";");
 const corsMiddleware = cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
-    // if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-    //     const message = "The CORS policy for this application doesn’t allow access from origin " + origin;
-    //     return callback(new Error(message), false);
-    // }
+    if (allowedOrigins.indexOf(origin) === -1) {
+      // If a specific origin isn’t found on the list of allowed origins
+      const message =
+        "The CORS policy for this application doesn’t allow access from origin " +
+        origin;
+      return callback(new Error(message), false);
+    }
     return callback(null, true);
   },
   credentials: true,
--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ require("./passport"); // Your local passport file
 const generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
     subject: user.username, // This is the username you’re encoding in the JWT
-    expiresIn: "10m", // This specifies that the token will expire in 7 days
+    expiresIn: "30m", // This specifies that the token will expire in 7 days
     algorithm: "HS256", // This is the algorithm used to “sign” or encode the values of the JWT
   });
 };
