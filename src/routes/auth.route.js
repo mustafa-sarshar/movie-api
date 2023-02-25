8109f1c746fb7d65863b170b16bcbdeee2e571ad
musto <musti.sar4@gmail.com>
2023-02-25 14:22:00 +0100

    Update Authentication code

---
+++ src/routes/auth.route.js
@@ -0,0 +1,16 @@
+/**
+ * @module authController
+ * @description This modules includes all methods related to authentication.
+ */
+const auth = require("../controllers/auth2.controller");
+
+require("../controllers/passport.controller"); // Your local passport file
+
+/* POST login. */
+/**
+ * Define the login route.
+ * @param {router} router
+ */
+module.exports = (router) => {
+  router.route("/login").post(auth);
+};
